8fde8454e45ab4f839d39d1d8cd9c5d592457be0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-11 11:33:51 -0400

    inteemediate revision - added home brewed cyrillic translit

--- src/test/java/com/github/sergueik/selenium/XPathNavigationTest.java
+++ src/test/java/com/github/sergueik/selenium/XPathNavigationTest.java
@@ -2,7 +2,10 @@ package com.github.sergueik.selenium;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -61,7 +64,7 @@ public class XPathNavigationTest extends BaseTest {
 
 	@Test(enabled = true)
 	public void test1() {
-		// does not work very  with headless browser
+		// does not work very with headless browser
 		// Arrange
 		String baseURL = "https://spb.rt.ru/packages/tariffs";
 		driver.get(baseURL);
@@ -90,6 +93,7 @@ public class XPathNavigationTest extends BaseTest {
 						// Подключить
 						// чить
 						// ь
+						assertThat(buttonElement.getText(), equalTo("Подключить"));
 						System.err.println(
 								String.format("Button Text: |%s|", buttonElement.getText()));
 						System.err.println(xpathOfElement(buttonElement));
@@ -103,7 +107,7 @@ public class XPathNavigationTest extends BaseTest {
 
 	// a debug version of test1.
 	// NOTE: slower
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void test2() {
 		// Arrange
 		String baseURL = "https://spb.rt.ru/packages/tariffs";
@@ -133,6 +137,12 @@ public class XPathNavigationTest extends BaseTest {
 										buttonElement);
 								highlight(buttonElement.findElement(By.xpath("..")));
 								System.err.println(String.format("Connection fee: %s", fee));
+								assertThat(buttonElement.getText(), equalTo("Подключить"));
+								// https://stackoverflow.com/questions/5806690/is-there-an-iconv-with-translit-equivalent-in-java
+								System.err.println(
+										String.format("Button Text assertion passed: |%s|%s|%s|",
+												buttonElement.getText(), "Подключить",
+												Translit.toAscii("Подключить")));
 								System.err.println(String.format("Button Text: |%s|",
 										buttonElement.getText()));
 								System.err.println(cssSelectorOfElement(buttonElement));
@@ -306,6 +316,65 @@ public class XPathNavigationTest extends BaseTest {
 				}
 			}
 		});
+	}
+
+	// based on https://stackoverflow.com/questions/5806690/is-there-an-iconv-with-translit-equivalent-in-java
+	// and http://tocrva.blogspot.com/2015/03/java-transliterate-cyrillic-to-latin.html
+	public static class Translit {
+
+		private static final Charset UTF8 = Charset.forName("UTF-8");
+		private static final char[] alphabetCyrillic = { ' ', 'а', 'б', 'в', 'г',
+				'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
+				'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю',
+				'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л',
+				'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ',
+				'Ъ', 'Ы', 'Б', 'Э', 'Ю', 'Я', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+				'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+				'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+				'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+				'Y', 'Z' };
+
+		private static final String[] alphabetTranslit = { " ", "a", "b", "v", "g",
+				"d", "e", "e", "zh", "z", "i", "y", "k", "l", "m", "n", "o", "p", "r",
+				"s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "", "i", "", "e",
+				"ju", "ja", "A", "B", "V", "G", "D", "E", "E", "Zh", "Z", "I", "Y", "K",
+				"L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "H", "Ts", "Ch", "Sh",
+				"Sch", "", "I", "", "E", "Ju", "Ja", "a", "b", "c", "d", "e", "f", "g",
+				"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+				"v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+				"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+				"X", "Y", "Z" };
+
+		private static String toAscii(final String input) {
+			final CharsetEncoder charsetEncoder = UTF8.newEncoder();
+			final char[] decomposed = Normalizer
+					.normalize(input, Normalizer.Form.NFKD).toCharArray();
+			final StringBuilder sb = new StringBuilder(decomposed.length);
+
+			// NOTE: evaluating the character charcount is unnecessary with Cyrillic
+			for (int i = 0; i < decomposed.length;) {
+				final int codePoint = Character.codePointAt(decomposed, i);
+				final int charCount = Character.charCount(codePoint);
+
+				if (charsetEncoder
+						.canEncode(CharBuffer.wrap(decomposed, i, charCount))) {
+					sb.append(decomposed, i, charCount);
+				}
+
+				i += charCount;
+			}
+
+			StringBuilder builder = new StringBuilder();
+
+			for (int i = 0; i < sb.length(); i++) {
+				for (int x = 0; x < alphabetCyrillic.length; x++)
+					if (sb.charAt(i) == alphabetCyrillic[x]) {
+						builder.append(alphabetTranslit[x]);
+					}
+			}
+			return builder.toString();
+
+		}
 	}
 
 }
