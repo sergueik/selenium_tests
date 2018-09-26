2a97fca1f41ab8418332bed73af5dd28c9c44ebf
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-25 22:51:42 -0400

    intermediate revision - fixed id by name getter setter results caching within PageElement class exampe (debug clean)

--- src/test/java/com/github/sergueik/selenium/FindingIdFromLabelForTest.java
+++ src/test/java/com/github/sergueik/selenium/FindingIdFromLabelForTest.java
@@ -61,29 +61,19 @@ public class FindingIdFromLabelForTest extends BaseTest {
 		pageElement.setDriver(driver);
 		pageElement.setWait(wait);
 		// Act
-		// will be initially empty, but getter would return an empty string
-		// commented temporarily because of the getter has a value caching side
-		// effect
-		try {
-			assertThat(pageElement.getValue(text1), equalTo(""));
-		} catch (java.lang.AssertionError e) {
-			assertThat(pageElement.getValue(text1), nullValue());
-		}
+		assertThat(pageElement.getValue(text1), nullValue());
 		pageElement.setValue(text1, "foo");
 		// Assert
 		String value1 = pageElement.getValue(text1);
 		assertThat(value1, equalTo("foo"));
 		System.err.println("New value: " + value1);
-		// Act
-		// commented temporarily because of the getter has a value caching side
-		// effect
-		/*
+
 		try {
-			assertThat(pageElement.getValue(text2), nullValue());
-		} catch (java.lang.AssertionError e) {
 			assertThat(pageElement.getValue(text2), equalTo(""));
+		} catch (java.lang.AssertionError e) {
+			assertThat(pageElement.getValue(text2), nullValue());
 		}
-		*/
+		// Act
 		pageElement.setValue(text2, "bar");
 		// Assert
 		String value2 = pageElement.getValue(text2);
@@ -124,13 +114,13 @@ public class FindingIdFromLabelForTest extends BaseTest {
 		private boolean debug = false;
 		private WebDriver driver;
 		private WebDriverWait wait;
-		private Actions actions;
 		private long timeout = 100;
 
 		private Map<String, String> cachedValues = new HashMap<>();
 		private List<WebElement> labelElements = new ArrayList<>();
 		private WebElement labelElement;
 		private WebElement inputElement;
+		private String selector = null;
 
 		public void setDebug(boolean value) {
 			this.debug = value;
@@ -147,27 +137,17 @@ public class FindingIdFromLabelForTest extends BaseTest {
 			this.driver = value;
 		}
 
-		public void setActions(Actions value) {
-			this.actions = value;
-		}
-
 		public void setWait(WebDriverWait value) {
 			this.wait = value;
 		}
 
 		public String getValue(String labelText) {
 			if (!cachedValues.containsKey(labelText)) {
-				if (debug)
-					System.err.println(
-							String.format("Looking for label with text %s", labelText));
 				labelElements = driver.findElements(By.xpath(
 						String.format("//label[contains(text(), '%s')]", labelText)));
 				assertTrue(labelElements.size() > 0);
 				labelElement = labelElements.get(0);
 				selector = labelElement.getAttribute("for");
-				if (debug)
-					System.err
-							.println(String.format("Label is for id: \"%s\"", selector));
 				try {
 					inputElement = driver.findElement(By.id(selector));
 					// Assert
@@ -176,59 +156,33 @@ public class FindingIdFromLabelForTest extends BaseTest {
 							? inputElement.getAttribute("value") : inputElement.getText();
 					if (debug)
 						System.err.println(String.format(
-								"Found by \"%s\" \"%s\" (in element named \"%s\")", selector,
-								valueText, inputElement.getAttribute("name")));
+								"Label with text \"%s\" is for input named \"%s\" with id \"%s\" value \"%s\"",
+								labelText, inputElement.getAttribute("name"), selector,
+								valueText));
 					// only cache non-empty values
-					if (debug)
-						System.err
-								.println(String.format("Inspecting value: \"%s\" null ? %s",
-										valueText, valueText != null ? "false" : "true"));
-					System.err.println(String.format("Inspecting value: \"%s\" \"\" ? %s",
-							valueText, valueText.trim().length() != 0 ? "false" : "true"));
 					if (valueText != null && valueText.trim().length() != 0) {
-						if (debug)
-							System.err
-									.println(String.format("Caching value: \"%s\"", valueText));
 						if (!(cachedValues.containsKey(labelText))) {
-							System.err.println(String.format("Caching: cache[\"%s\"]=\"%s\"",
-									labelText, valueText));
 							cachedValues.put(labelText, valueText);
 						} else {
-							if (debug)
-								System.err.println(
-										String.format("Updating cache: cache[\"%s\"]=\"%s\"",
-												labelText, valueText));
 							cachedValues.replace(labelText, valueText);
 						}
 					}
 				} catch (NoSuchElementException e) {
-					System.err.println(String.format(
-							"Selenium search by id %s leads to exception: %s...", selector,
-							e.toString().substring(0, 200)));
+					System.err.println(String.format("Exception (caught): %s...",
+							selector, e.toString().substring(0, 200)));
 				}
-			}
-			if (debug) {
-				System.err.println("Returning " + cachedValues);
-				System.err.println("Returning " + cachedValues.get(labelText));
 			}
 			return cachedValues.containsKey(labelText) ? cachedValues.get(labelText)
 					: null;
 		}
 
-		private String selector = null;
-
 		public void setValue(String labelText, String value) {
 
-			if (debug)
-				System.err.println(
-						String.format("Looking for label with text %s", labelText));
 			labelElements = driver.findElements(By
 					.xpath(String.format("//label[contains(text(), '%s')]", labelText)));
 			assertTrue(labelElements.size() > 0);
 			labelElement = labelElements.get(0);
 			selector = labelElement.getAttribute("for");
-			if (debug)
-				System.err.println(String.format("Label is for id: \"%s\"", selector));
 			try {
 				inputElement = driver.findElement(By.id(selector));
 				assertThat(inputElement, notNullValue());
@@ -242,12 +196,12 @@ public class FindingIdFromLabelForTest extends BaseTest {
 						? inputElement.getAttribute("value") : inputElement.getText();
 				assertThat(valueText, equalTo(value));
 				if (debug)
-					System.err.println(String.format("%s set to %s", selector,
-							inputElement.getAttribute("value")));
+					System.err.println(String.format(
+							"Label with text \"%s\" is for input with id: \"%s\" value set to \"%s\"",
+							labelText, selector, inputElement.getAttribute("value")));
 			} catch (NoSuchElementException e) {
-				System.err.println(
-						String.format("Selenium search by id %s leads to exception: %s...",
-								selector, e.toString().substring(0, 200)));
+				System.err.println(String.format("Exception (caught): %s...", selector,
+						e.toString().substring(0, 200)));
 			}
 
 		}
