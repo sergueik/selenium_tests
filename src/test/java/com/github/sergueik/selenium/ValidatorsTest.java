34d3ce8447797ca628964f1aa864a3eb1c8373dc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-12-15 12:13:37 -0500

    intermediate revision - made tests data parameterezed, found a small fix needed to CssSelector condition extractor, put in DataProvider(name = "Unrecognized Valid CssSelector Conditions")

--- src/test/java/com/github/sergueik/selenium/ValidatorsTest.java
+++ src/test/java/com/github/sergueik/selenium/ValidatorsTest.java
@@ -4,29 +4,9 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.testng.annotations.Test;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -36,52 +16,49 @@ import org.testng.annotations.Test;
 
 public class ValidatorsTest {
 
-	private boolean debug = true;
+	private boolean debug = false;
 	private static final CssValidator cssValidator = CssValidator.getInstance();
 	private static final XPathValidator xpathValidator = XPathValidator
 			.getInstance();
 
-	@SuppressWarnings("deprecation")
 	@BeforeClass
 	public void beforeClass() throws IOException {
 		xpathValidator.setDebug(debug);
 		cssValidator.setDebug(debug);
 	}
 
-	@SuppressWarnings("unused")
-	@Test(enabled = true)
-	public void xpathComprehensiveTokenPositiveTest() {
-		String xpathString = "a[@class='main']/b//c[@class='main']";
-		assertTrue(xpathValidator.comprehensiveTokenTest(xpathString));
+	@Test(dataProvider = "Valid XPaths", enabled = true)
+	public void xpathComprehensiveTokenPositiveTest(String xpath) {
+		assertTrue(xpathValidator.comprehensiveTokenTest(xpath),
+				String.format("\"%s\"to be valid XPath", xpath));
 	}
 
-	@SuppressWarnings("unused")
-	@Test(enabled = true)
-	public void xpathComprehensiveTokenNegativeTest() {
-		String cssSelectorString = "body > h1[name='hello'] h2:nth-of-type(1) div";
-		assertFalse(xpathValidator.comprehensiveTokenTest(cssSelectorString));
+	@Test(dataProvider = "Valid CssSelectors", enabled = true)
+	public void xpathComprehensiveTokenNegativeTest(String cssSelector) {
+		assertFalse(xpathValidator.comprehensiveTokenTest(cssSelector),
+				String.format("the CSS selector \"%s\"to be valid XPath (not really)",
+						cssSelector));
 	}
 
-	@Test(enabled = true)
-	public void xpathComprehensiveTokenNegativeTextTest() {
-		String textString = "hello world";
+	@Test(dataProvider = "Texts", enabled = true)
+	public void xpathComprehensiveTokenNegativeTextTest(String textString) {
 		assertFalse(xpathValidator.comprehensiveTokenTest(textString));
 	}
 
-	@Test(enabled = true)
-	public void cssSelectorComprehensiveTokenPositiveTest() {
-		String cssSelectorString = "body > h1[name='hello'] h2:nth-of-type(1) div";
-		assertFalse(cssValidator.comprehensiveTokenTest(cssSelectorString));
+	@Test(dataProvider = "Valid CssSelectors", enabled = true)
+	public void cssSelectorComprehensiveTokenPositiveTest(String cssSelector) {
+		assertFalse(cssValidator.comprehensiveTokenTest(cssSelector),
+				String.format("\"%s\"to be valid CSS selector", cssSelector));
 	}
 
-	@Test(enabled = true)
-	public void cssSelectorComprehensiveTokenNegativeTest() {
-		String xpathString = "a[@class='main']/b//c[@class='main']";
-		assertFalse(cssValidator.comprehensiveTokenTest(xpathString));
+	@Test(dataProvider = "Valid XPaths", enabled = true)
+	public void cssSelectorComprehensiveTokenNegativeTest(String xpath) {
+		assertFalse(cssValidator.comprehensiveTokenTest(xpath), String.format(
+				"the XPath \"%s\"to be valid CSS selector (not really)", xpath));
 	}
 
-	// NOTE: will fail to fail unless
-	// one enforces some ad.hoc conditions on cssSelector to be 
+	// NOTE: The test will fail to fail unless
+	// one enforces some ad.hoc conditions on cssSelector to be
 	// distinguishable from the plain English text,
 	// like e.g. enforcing token to always contain common
 	// page tag names "a", "td", "div", "span", "input" etc.
@@ -92,75 +69,74 @@ public class ValidatorsTest {
 		assertFalse(cssValidator.comprehensiveTokenTest(textString));
 	}
 
-	@Test(enabled = false)
-	public void xPathTokenTest() {
-		String xpathString = "a/b//c";
-		assertTrue(xpathString.matches(xpathValidator.getTokenValidator()));
+	@Test(dataProvider = "Valid XPaths", enabled = true)
+	public void xPathTokenTest(String xpathString) {
+		assertTrue(xpathString.matches(xpathValidator.getTokenValidator()),
+				String.format("\"%s\"to be valid XPath", xpathString));
 	}
 
-	@Test(enabled = false)
-	public void xPathTokenTest2() {
-		// TODO: passes because it is assumed to be one token
-		String xpathString = "a/b//c[@class='main']";
-		assertTrue(xpathString.matches(xpathValidator.getTokenValidator()));
-	}
-
-	@Test(enabled = false)
-	public void xPathTokenTest3() {
-		// TODO: passes because it is assumed to be one token
-		String xpathString = "//a[@class='main']";
-		assertTrue(xpathString.matches(xpathValidator.getTokenValidator()));
-	}
-
-	@Test(enabled = false)
-	public void xPathTokenTest4() {
-		String xpathString = "//a[contains(@class,'main')]/b[@id]/d[1]/..";
-		assertTrue(xpathString.matches(xpathValidator.getTokenValidator()));
-	}
-
-	// TODO: finish
-	@Test(enabled = false)
-	public void xpathAttributeConditonTest() {
-		String xpath = "a[@name = 'home']";
-		assertFalse(xpath.matches(xpathValidator.getAttributeValidator()));
-	}
-
-	// TODO: finish
-	@Test(enabled = false)
-	public void xpathTextConditonTest() {
-		String xpath = "a[contains(text(), 'home')]";
-		assertTrue(xpath.matches(xpathValidator.getAttributeValidator()));
-	}
-
-	@Test(enabled = false)
-	public void cssSelectorTokenTest() {
-		String cssSelector = "a.class > b#id c:nth-of-type(1)";
+	@Test(dataProvider = "Valid CssSelectors", enabled = true)
+	public void cssSelectorTokenTest(String cssSelector) {
 		assertTrue(cssSelector.matches(cssValidator.getTokenValidator()));
 	}
 
-	// NOTE: fails to fail
-	// passes because it is assumed to be one token
-	@Test(enabled = false)
-	public void xPathTokenBadTest() {
-		String xpath = "a/b//c[@class='main']";
-		assertFalse(xpath.matches(cssValidator.getTokenValidator()));
+	// NOTE: passes because it is assumed to be single token.
+	// WORKAROUND: Perform XPath probing first
+	@Test(dataProvider = "Valid XPath Conditions", enabled = false)
+	public void CssLocatorTokenBadTest(String xpathCondition) {
+		assertFalse(xpathCondition.matches(cssValidator.getTokenValidator()));
 	}
 
-	@Test(enabled = false)
-	public void cssSelectorAttributeTest() {
-		String cssSelector = "a[name*='home']";
-		assertTrue(cssSelector.matches(cssValidator.getAttributeValidator()));
+	@Test(dataProvider = "Valid CssSelector Conditions", enabled = true)
+	public void cssSelectorConditionTest(String cssSelectorCondition) {
+		assertTrue(
+				cssSelectorCondition.matches(cssValidator.getAttributeValidator()),
+				String.format("\"%s\"to be valid condition in CSS selector",
+						cssSelectorCondition));
 	}
 
-	@Test(enabled = false)
-	public void xpathAttributeTest1() {
-		String xpath = "a[@name = 'home']";
-		assertFalse(xpath.matches(cssValidator.getAttributeValidator()));
+	// Valid cssSelectors data
+	@DataProvider(name = "Valid CssSelectors")
+	public Object[][] Data1() {
+		return new Object[][] { { "a.class > b#id c:nth-of-type(1)" },
+				{ "div.class ~ input#id" },
+				{ "body > h1[name='hello'] h2:nth-of-type(1) div" }, };
 	}
 
-	@Test(enabled = false)
-	public void xpathAttributeTest2() {
-		String xpath = "a[contains(text(), 'home')]";
-		assertFalse(xpath.matches(cssValidator.getAttributeValidator()));
+	// Valid cssSelector condition
+	@DataProvider(name = "Valid CssSelector Conditions")
+	public Object[][] Data2() {
+		return new Object[][] { { "div:nth-of-type(1)" },
+				{ "input[type='submit']" }, { "div[class*='ng']" },
+				{ "h1[name='hello']" }, { "input[name^='Pass']" }, };
+	}
+
+	// TODO: need to modify Condition extractor to allow simple id and class
+	// TODO: also mis-recognized as a valid XPath
+	@DataProvider(name = "Unrecognized Valid CssSelector Conditions")
+	public Object[][] Data2B() {
+		return new Object[][] { { "input#id" }, { "div.class" },
+				// derivatives
+				{ "input#id[name^='Pass']" }, { "input.class[name^='Pass']" }, };
+	}
+
+	// Valid XPath data
+	@DataProvider(name = "Valid XPaths")
+	public Object[][] Data10() {
+		return new Object[][] { { "a[@class='main']/b//c[@class='main']" },
+				{ "/body//td/following-sibling::td[1]" }, { "/tr[0]/../th" } };
+	}
+
+	// Valid XPath condition
+	@DataProvider(name = "Valid XPath Conditions")
+	public Object[][] Data3() {
+		return new Object[][] { { "a[contains(text(), 'home')]" }, { "//a/b[1]/c" },
+				{ "//a[contains(@class,'main')]/b[@id]/d[1]/.." }, };
+	}
+
+	// Plain Text samples
+	@DataProvider(name = "Texts")
+	public Object[][] Data5() {
+		return new Object[][] { { "Hello World" } };
 	}
 }
