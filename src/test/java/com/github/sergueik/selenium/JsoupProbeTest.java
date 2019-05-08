1b69977880925a2556f9e90658c7a26228b2c040
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-08 00:38:51 -0400

    intermeidate revision - building efficient (?) jsoup traversal strategy

--- src/test/java/com/github/sergueik/selenium/JsoupProbeTest.java
+++ src/test/java/com/github/sergueik/selenium/JsoupProbeTest.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Formatter;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.testng.ITestResult;
@@ -66,7 +71,7 @@ import static org.hamcrest.Matchers.greaterThan;
 public class JsoupProbeTest extends BaseTest {
 
 	private static final Logger log = LogManager.getLogger(JsoupProbeTest.class);
-	private String text = "A2300";
+	private String filePath = "links.htm";
 	// private String selector = "#acListWrap > div:nth-child(3) > div >
 	// div.productListingMiddle > div.lower.tabsection";
 	private String selector = "#acListWrap div.productListing h2.listSubtitle > a[href*='/item/'][title='Click to View']";
@@ -80,12 +85,13 @@ public class JsoupProbeTest extends BaseTest {
 
 	@BeforeMethod
 	public void loadPage() {
-		driver.navigate().to(getPageContent("links.htm"));
-		pageSource = driver.getPageSource();
+		pageSource = getScriptContent(filePath);
 	}
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void testPageVisual() {
+		driver.navigate().to(getPageContent(filePath));
+		pageSource = driver.getPageSource();
 		List<WebElement> elements = driver.findElements(By.cssSelector(selector));
 		assertThat(elements, notNullValue());
 		assertThat(elements.size(), greaterThan(1));
@@ -107,6 +113,99 @@ public class JsoupProbeTest extends BaseTest {
 		assertThat(divjsoupElements.eachText().size(), greaterThan(1));
 		System.err.println(String.format("Processing attribute(\"%s\") = \"%s\" %s",
 				attributeName, attributeValue, divjsoupElements.first().text()));
+	}
+
+	@Test(enabled = true)
+	public void testBroken2() {
+		Document jsoupDocument = Jsoup.parse(pageSource);
+
+		final List<Map<String, String>> attrMap = new ArrayList<>();
+		Map<String, String> attrStep = new HashMap<>();
+		attrStep.put("id", "acListWrap");
+		attrMap.add(attrStep);
+		// attrStep.remove("id");
+
+		attrStep.put("class", "productListing");
+		attrMap.add(attrStep);
+		// attrStep.remove("class");
+		attrStep.put("class", "listSubtitle");
+		attrMap.add(attrStep);
+		// attrStep.remove("class");
+		attrStep.put("title", "Click to View");
+		attrMap.add(attrStep);
+		// attrStep.remove("title");
+		System.err.println("Processing " + attrMap.toString());
+
+		Iterator<Map<String, String>> attrMapIterator = attrMap.iterator();
+		Document parentDocument = jsoupDocument;
+		while (attrMapIterator.hasNext()) {
+			attrStep = attrMapIterator.next();
+			System.err.println("Processing " + attrStep.toString());
+			/*
+			Set<String> attributeKeys = attrStep.keySet();
+			assertThat(attributeKeys.size(), greaterThan(0));
+			String attributeName = attributeKeys.toArray()[0].toString();
+			String attributeValue = attrStep.get(attributeName);
+			Elements jsoupElements = parentDocument
+					.getElementsByAttributeValue(attributeName, attributeValue);
+			
+			// Assert
+			assertThat(jsoupElements, notNullValue());
+			assertThat(jsoupElements.iterator().hasNext(), is(true));
+			assertThat(jsoupElements.eachText().size(), greaterThan(1));
+			System.err
+					.println(String.format("Processing attribute(\"%s\") = \"%s\" %s",
+							attributeName, attributeValue, jsoupElements.first().text()));
+			parentDocument = (Document) jsoupElements.first();
+			*/
+		}
+	}
+
+	@Test(enabled = true)
+	public void testDeepPageSource() {
+		Document jsoupDocument = Jsoup.parse(pageSource);
+
+		final List<String> attrKeys = Arrays
+				.asList(new String[] { "id", "class", "class", "title" });
+		final List<String> attrValues = Arrays.asList(new String[] { "acListWrap",
+				"productListing", "listSubtitle", "Click to View" });
+		Document parentDocument = jsoupDocument;
+
+		for (int pos = 0; pos != attrKeys.size(); pos++) {
+			System.err.println(
+					"Processing " + attrKeys.get(pos) + ", " + attrValues.get(pos));
+			String attributeName = attrKeys.get(pos);
+			String attributeValue = attrValues.get(pos);
+			Elements jsoupElements = parentDocument
+					.getElementsByAttributeValue(attributeName, attributeValue);
+
+			// Assert
+			assertThat(jsoupElements, notNullValue());
+			assertThat(jsoupElements.iterator().hasNext(), is(true));
+			assertThat(jsoupElements.eachText().size(), greaterThan(0));
+			System.err
+					.println(String.format("Processing attribute(\"%s\") = \"%s\" %s",
+							attributeName, attributeValue, jsoupElements.first().text()));
+			parentDocument = Jsoup.parse(jsoupElements.first().text());
+		}
+	}
+
+	// LinkedHashMap does not work
+	@Test(enabled = true)
+	public void testBroken() {
+		Document jsoupDocument = Jsoup.parse(pageSource);
+
+		final LinkedHashMap<String, String> attrMap = new LinkedHashMap<>();
+		attrMap.put("id", "acListWrap");
+		attrMap.put("class", "productListing");
+		attrMap.put("class", "listSubtitle");
+		attrMap.put("title", "Click to View");
+
+		final List<String> attrKeys = new ArrayList<>();
+		for (Entry<String, String> o : attrMap.entrySet()) {
+			attrKeys.add(o.getKey());
+		}
+		System.err.println(attrKeys.toString());
 	}
 
 }
