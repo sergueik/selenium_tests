254409899bdeb11a93e6189ff5e8386488d90275
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-17 00:15:08 -0400

    itermediate reviion - added Perl version, inline

--- src/test/java/com/github/sergueik/selenium/JsoupProbe2Test.java
+++ src/test/java/com/github/sergueik/selenium/JsoupProbe2Test.java
@@ -1,69 +1,30 @@
 package com.github.sergueik.selenium;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Paths;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Formatter;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-
-import static org.junit.Assert.assertTrue;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
-
-import static org.hamcrest.Matchers.greaterThan;
-import java.util.regex.Pattern;
-
 /**
-* Sample test scenario for web page scraping via joup based on chained  fast track node attribute scan
-* that is a lot less code than chained browse of immediate (grand-) children
+* Sample test scenario for web page scraping via joup based on chained node attribute scan
+* that is more precise than browsing of immediate (grand-) children
 * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
 */
 
@@ -79,41 +40,45 @@ public class JsoupProbe2Test extends BaseTest {
 
 	private static boolean debug = false;
 	private static final Logger log = LogManager.getLogger(JsoupProbe2Test.class);
-	private String filePath = "1-acre-sale-several-greenhouses-residence-oneco-ct.html";
+	private String filePath = "list2.html";
 	// origin:
 	// https://newenglandfarmlandfinder.org/property/1-acre-sale-several-greenhouses-residence-oneco-ct
 	// for visual inspection
-	// private String selector = "div.region-content fieldset.group-info div.field-body";
+	// private String selector = "div.region-content fieldset.group-info
+	// div.field-body";
 	// verified
 	private String selector = "div.region-content fieldset.group-property-tenure div.field-sale-price";
 	private String pageSource = null;
 	private static Document jsoupDocument;
 	private static List<WebElement> elements;
+	private final Map<String, Map<String, List<String>>> locatorChains = new HashMap<>();
+	// TODO: (de)serialize through YAML
 	private final List<String> attrKeys = Arrays
 			.asList(new String[] { "class", "class", "class" });
-	// NOTE: with leading space in attribute, do not like attribute filtering by
-	// exact value
+	// NOTE: with a leading space in the DOM element attribute,
 	// " group-info field-group-fieldset form-wrapper"
-	// but https://metacpan.org/pod/HTML::TagParser has no equivalent
+	// one would like to
+	// do attribute filtering by exact value
+	// but the HTML::TagParser Perl module has no equivalent
 	// functionality
 	/*
 	private final List<String> attrValues = Arrays
 			.asList(new String[] { "region-content", "group-info", "field-body" });
-
+	
 	private final List<String> attrValuesExact = Arrays
 			.asList(new String[] { "region-content",
 					" group-info field-group-fieldset form-wrapper", "field-body" });
 	*/
 	// verified
 
-	private final List<String> attrValues = Arrays
-			.asList(new String[] { "region-content", "group-property-tenure", "field-sale-price" });
+	private final List<String> attrValues = Arrays.asList(new String[] {
+			"region-content", "group-property-tenure", "field-sale-price" });
 
 	private final List<String> attrValuesExact = Arrays
 			.asList(new String[] { "region-content",
-					" group-property-tenure field-group-fieldset form-wrapper", "field-sale-price" });
+					" group-property-tenure field-group-fieldset form-wrapper",
+					"field-sale-price" });
 
-	
 	private static Document parentDocument;
 	private static Elements jsoupElements;
 
@@ -175,6 +140,9 @@ public class JsoupProbe2Test extends BaseTest {
 							String.format("Processing attribute(\"%s\") = \"%s\" %s...",
 									attributeName, attributeValue, innerHTML.substring(0, 160)));
 				}
+				System.err.println(String.format("Full attribute: \"%s\" = \"%s\"",
+						attributeName, jsoupElements.first().attr(attributeName)));
+
 				parentDocument = Jsoup.parse(innerHTML);
 			}
 		}
@@ -217,31 +185,87 @@ public class JsoupProbe2Test extends BaseTest {
 	}
 	/*
 	 This can be used with Perl
-	#!/usr/bin/perl
 	
-	use warnings;
-	use strict;
+		#!/usr/bin/perl
 	
-	use Data::Dumper qw(Dumper);
-	use HTML::TagParser;
-	
-	use vars qw($DEBUG);
-	
-	$element =
-	( HTML::TagParser->new('links.htm')->getElementsByAttribute( 'id', 'acListWrap' ) )[0]->subTree();
-	
-	sub getData($$$) {
-	  my ( $e, $n, $v ) = @_;
-	  my @e = $e->getElementsByAttribute( $n, $v );
-	  my @d =
-	    map { my $t = $_->innerText; $t =~ s|\s+| |g; $t } @e;
-	  \@d;
-	}
-	
-	print Dumper 	\{
-		'price' => getData( $element, 'class', 'acListPrice' ),
-		'title' => getData( $element, 'class', 'listSubtitle' ),
-		'description'=> getData( $element, 'class', 'lower tabsection' ),
-	};	
+		use warnings;
+		use strict;
+		
+		use Getopt::Long;
+		use Data::Dumper qw(Dumper);
+		use List::Util qw(max);
+		use HTML::TagParser;
+		
+		use vars qw($DEBUG $MAX $DATA);
+		$DEBUG = 0;
+		
+		sub getSubTree($$$) {
+		    my ( $e, $n, $v ) = @_;
+		    my @e = $e->getElementsByAttribute( $n, $v );
+		    $e[0]->subTree();
+		}
+		
+		sub getData($$$) {
+		    my ( $e, $n, $v ) = @_;
+		    my @e = $e->getElementsByAttribute( $n, $v );
+		    my @d =
+		      map { my $t = $_->innerText; $t =~ s|\s+| |g; $t } @e;
+		    \@d;
+		}
+		my $filename = 'list2.html';
+		
+		my $locatorChains = {
+		    'info' => {
+		        'names'  => [ 'class', 'class', 'class' ],
+		        'values' => [
+		            'region-content', ' group-info field-group-fieldset form-wrapper',
+		            'field-body'
+		        ]
+		    },
+		    'land_area' => {
+		        'names'  => [ 'class', 'class', 'class' ],
+		        'values' => [
+		            'region-content',
+		            ' group-property-land field-group-fieldset form-wrapper',
+		            'field-acres-total inline'
+		        ]
+		    },
+		
+		    'price' => {
+		        'names'  => [ 'class', 'class', 'class' ],
+		        'values' => [
+		            'region-content',
+		            ' group-property-tenure field-group-fieldset form-wrapper',
+		            'field-sale-price'
+		        ]
+		    }
+		};
+		my $results = {};
+		
+		foreach my $entry ( keys %$locatorChains ) {
+		    $results->{$entry} = undef;
+		    my $element = HTML::TagParser->new($filename);
+		    my $names   = $locatorChains->{$entry}->{'names'};
+		    my $values  = $locatorChains->{$entry}->{'values'};
+		    if ($DEBUG) {
+		        print Dumper($names);
+		        print Dumper($values);
+		    }
+		    foreach my $step ( 0 ... $#$names ) {
+		        if ( $step == $#$names ) {
+		            my $data = getData( $element, $names->[$step], $values->[$step] );
+		            print Dumper \$data if $DEBUG;
+		            $results->{$entry} = $data->[0];
+		        }
+		        else {
+		            $element =
+		              getSubTree( $element, $names->[$step], $values->[$step] );
+		        }
+		        print $step if $DEBUG;
+		    }
+		}
+		
+		print Dumper \$results;
+		
 	 */
 }
