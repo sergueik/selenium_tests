6f8615847af92bdc86c7f283663651dcd20f6f84
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-05-27 17:46:52 -0400

    intermediate revision - added shadow DOM example from www.seleniumeasy.com

---
+++ src/test/java/com/github/sergueik/selenium/ShadowDOMTest.java
@@ -0,0 +1,123 @@
+package com.github.sergueik.selenium;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+// based on: https://www.seleniumeasy.com/selenium-tutorials/accessing-shadow-dom-elements-with-webdriver
+public class ShadowDOMTest extends BaseTest {
+
+	private static final Logger log = LogManager.getLogger(ShadowDOMTest.class);
+	private String baseURL = "chrome://downloads/";
+	// origin:http://www.louisianaoutdoorproperties.com
+	private static Document jsoupDocument;
+	private static WebElement element1, element2, element3;
+	private static WebElement root1, root2, root3;
+
+	@BeforeClass
+	public void beforeClass() throws IOException {
+		super.setBrowser("chrome");
+		super.beforeClass();
+		assertThat(driver, notNullValue());
+	}
+
+	@BeforeMethod
+	public void loadPage() {
+		System.err.println("Open Chrome downloads");
+		driver.get(baseURL);
+	}
+
+	@Test
+	public void shadowDOMElementsTest() {
+
+		System.err.println("Validate downloads page header text");
+		element1 = driver.findElement(By.tagName("downloads-manager"));
+
+		System.err.println("element html: " + element1.getAttribute("outerHTML"));
+
+		// Get shadow root element
+		WebElement root1 = expandRootElement(element1);
+		assertThat(root1, notNullValue());
+		try {
+			assertThat(root1.getText(), notNullValue());
+			System.err.println("Root1 text: " + root1.getText());
+		} catch (JavascriptException e) {
+			// Exception (ignored) org.openqa.selenium.JavascriptException: javascript
+			// error: Failed to execute 'getComputedStyle' on 'Window': parameter 1 is
+			// not of type 'Element'.
+			System.err.println("Exception (1) (ignored) " + e.getMessage());
+		}
+		System.err.println(root1.toString());
+		// [org.openqa.selenium.remote.RemoteWebElement@7de7299a -> unknown locator]
+
+		try {
+			jsoupDocument = Jsoup.parse(root1.getAttribute("outerHTML"));
+			System.err.println(jsoupDocument.ownText());
+		} catch (JavascriptException e) {
+			// org.openqa.selenium.JavascriptException:
+			// javascript error: a.getAttributeNode is not a function
+			System.err.println("Exception (2) (ignored) " + e.getMessage());
+		}
+		// Element members : null
+		System.err.println("Element members : " + executeScript(
+				"let x = arguments[0]; let result = []; for (y in x) {result.push(y);} JSON.stringify(result)",
+				root1));
+
+		element2 = root1.findElement(By.cssSelector("downloads-toolbar"));
+		root2 = expandRootElement(element2);
+		assertThat(root2, notNullValue());
+		try {
+			jsoupDocument = Jsoup.parse(root2.getAttribute("outerHTML"));
+			System.err.println(jsoupDocument.ownText());
+		} catch (JavascriptException e) {
+			// org.openqa.selenium.JavascriptException:
+			// javascript error: a.getAttributeNode is not a function
+			System.err.println("Exception (3) (ignored) " + e.getMessage());
+		}
+
+		element3 = root2.findElement(By.cssSelector("cr-toolbar"));
+		root3 = expandRootElement(element3);
+		assertThat(root3, notNullValue());
+		try {
+			jsoupDocument = Jsoup.parse(root3.getAttribute("outerHTML"));
+			System.err.println(jsoupDocument.html());
+		} catch (JavascriptException e) {
+			// javascript error: a.getAttributeNode is not a function
+			System.err.println("Exception (4) (ignored) " + e.getMessage());
+		}
+
+		try {
+			String actualHeading = root3
+					.findElement(By.cssSelector("div[id=leftContent]")).getText();
+			/* "div[id=leftContent]>h1"*/
+			// Verify header title
+			assertEquals("Downloads", actualHeading);
+		} catch (NoSuchElementException e) {
+			// NoSuchElementException NoSuchElement no such element
+			System.err.println("Exception (5) (ignored) " + e.getMessage());
+		}
+		System.err.println("stringify: " + executeScript(
+				"return JSON.stringify(arguments[0].shadowRoot)", element3));
+	}
+
+	// Cast shadow Root to WebElement
+	public WebElement expandRootElement(WebElement element) {
+		return (WebElement) executeScript("return arguments[0].shadowRoot",
+				element);
+	}
+
+}
