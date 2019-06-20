a78c021692f392d2ed18b733fdfc5006948d6f3a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-06-20 17:04:04 -0400

    intermediate revision - added generic and extraction with Snakeaml and Jackson working together

--- src/test/java/com/github/sergueik/selenium/JacksonDummyHtmlUnitTest.java
+++ src/test/java/com/github/sergueik/selenium/JacksonDummyHtmlUnitTest.java
@@ -11,16 +11,21 @@ import static org.testng.Assert.assertTrue;
 import java.util.Map;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import org.testng.annotations.Test;
+import org.yaml.snakeyaml.Yaml;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -72,8 +77,47 @@ public class JacksonDummyHtmlUnitTest {
 		} catch (/* IOException e|| JsonParseException e || JsonMappingException e */ Exception e) {
 
 		}
-		System.err.println(
-				String.format("JSON serialization with Jackson: \n%s\n", jsonString));
+		System.err.println(String.format("testYAMLtoJSON:\n%s\n", jsonString));
+	}
+
+	@Test(enabled = true)
+	public void testLoadGenericYAML() {
+		String fileName = String.join(System.getProperty("file.separator"),
+				Arrays.asList(System.getProperty("user.dir"), "src", "test",
+						"resources", "generic.yaml"));
+		InputStream in;
+		try {
+			// load with snakeyaml
+			in = Files.newInputStream(Paths.get(fileName));
+			// hack: direct parsing into a real Java object fails with
+			// SnakeYaml, Gson and Jackson due to strongly typed
+			Map<String, Object> data = (Map<String, Object>) new Yaml().load(in);
+			// java.lang.ClassCastException
+			Map<String, Object> userData = (Map<String, Object>) data.get("user");
+			ArrayList<String> roles = (ArrayList<String>) userData.get("roles");
+			// Exception (ignored):
+			// com.fasterxml.jackson.databind.JsonMappingException: java.lang.Integer
+			// cannot be cast to java.lang.String (through reference chain:
+			// com.github.sergueik.selenium.JacksonDummyHtmlUnitTest$User["address"]->java.util.LinkedHashMap["zip"])
+			User user = new User((String) userData.get("name"),
+					(String) userData.get("familyname"),
+					// java.lang.ClassCastException: java.lang.Integer cannot be cast to
+					// java.lang.String
+					/* Integer.parseInt((String) userData.get("age")),*/
+					(int) userData.get("age"),
+					(Map<String, Object>) userData.get("address"),
+					// java.lang.ClassCastException: java.util.ArrayList cannot be cast to
+					// [Ljava.lang.String;
+					(String[]) roles.toArray(new String[roles.size()]));
+			// dump with Jackson
+			jsonString = ouputObjectMapper.writeValueAsString(user);
+			System.err.println(String.format(
+					"Cherry-picking the user with SnakeYaml and Jackson: \n%s\n",
+					jsonString));
+
+		} catch (IOException e) {
+			System.err.println("Exception (ignored): " + e.toString());
+		}
 	}
 
 	@Test(enabled = true)
@@ -88,19 +132,28 @@ public class JacksonDummyHtmlUnitTest {
 			assertThat(user.getName(), is("Test User"));
 			assertThat(user.getAddress().get("line2"), nullValue());
 			assertThat(user.getRoles().length, greaterThan(1));
-			System.err.println(ReflectionToStringBuilder.toString(user,
-					ToStringStyle.MULTI_LINE_STYLE));
+			System.err.println("testLoadYAML:\n" + ReflectionToStringBuilder
+					.toString(user, ToStringStyle.MULTI_LINE_STYLE));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("Exception (ignored): " + e.toString());
 		}
 	}
 
 	private static class User {
+
 		private String name;
+		private String familyname;
 		private int age;
-		private Map<String, String> address;
+		private Map<String, Object> address;
 		private String[] roles;
+
+		public String getFamilyname() {
+			return familyname;
+		}
+
+		public void setFamilyname(String data) {
+			this.familyname = data;
+		}
 
 		public String getName() {
 			return name;
@@ -118,11 +171,11 @@ public class JacksonDummyHtmlUnitTest {
 			this.age = data;
 		}
 
-		public Map<String, String> getAddress() {
+		public Map<String, Object> getAddress() {
 			return address;
 		}
 
-		public void setAddress(Map<String, String> data) {
+		public void setAddress(Map<String, Object> data) {
 			this.address = data;
 		}
 
@@ -133,5 +186,21 @@ public class JacksonDummyHtmlUnitTest {
 		public void setRoles(String[] data) {
 			this.roles = data;
 		}
+
+		// default constructor neeeded for jackson
+		public User() {
+
+		}
+
+		public User(String name, String familyname, int age,
+				Map<String, Object> address, String[] roles) {
+			super();
+			this.name = name;
+			this.familyname = familyname;
+			this.age = age;
+			this.address = address;
+			this.roles = roles;
+		}
 	}
+
 }
