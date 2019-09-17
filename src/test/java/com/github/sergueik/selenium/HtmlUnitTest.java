2552cd255d5360a83180fde307f20e673c806a3b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-09-17 09:55:38 -0400

    intermediate revision - mostly cosmetic modifications, aded object in container deserialization tests

--- src/test/java/com/github/sergueik/selenium/HtmlUnitTest.java
+++ src/test/java/com/github/sergueik/selenium/HtmlUnitTest.java
@@ -5,12 +5,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.testng.Assert.assertTrue;
 
+import java.net.URLEncoder;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.DomNode;
@@ -50,7 +52,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 // https://futurestud.io/tutorials/gson-mapping-of-arrays-and-lists-of-objects
 
 // for YAML alone
-import com.fasterxml.jackson.databind.ObjectMapper;
+// import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 
@@ -70,8 +72,8 @@ import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 /**
-* Sample test scenario for web page scraping with HTMLUnit
-based on https://github.com/ksahin/introWebScraping
+* Sample test scenario typical for web page scraping with HTMLUnit
+* based on https://github.com/ksahin/introWebScraping
 * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
 */
 
@@ -83,10 +85,10 @@ public class HtmlUnitTest extends BaseTest {
 
 	private static boolean debug = false;
 
-	private final static boolean directConvertrsion = true;
+	private final static boolean directConversion = true;
 	// plain 1/1 conversion
 	// https://futurestud.io/tutorials/gson-getting-started-with-java-json-serialization-deserializationhttps://futurestud.io/tutorials/gson-getting-started-with-java-json-serialization-deserializationhttps://futurestud.io/tutorials/gson-getting-started-with-java-json-serialization-deserialization
-	Gson gson = directConvertrsion ? new Gson()
+	Gson gson = directConversion ? new Gson()
 			: new GsonBuilder()
 					.registerTypeAdapter(ObjectItem.class, new ObjectItemSerializer())
 					.create();
@@ -102,7 +104,7 @@ public class HtmlUnitTest extends BaseTest {
 	}
 	private static ObjectMapper mapper2Yaml = new ObjectMapper(yamlFactory);
 
-	private static final boolean prettify = true;
+	private static final boolean prettyPrint = true;
 	private static ObjectItem objItem;
 	private static String jsonString = null;
 	private static String yamlString = null;
@@ -133,13 +135,13 @@ public class HtmlUnitTest extends BaseTest {
 	private WebElement element;
 	private String pageSource = null;
 
-	// retrievable thru HTMLUnit XML methods
+	// collect via HTMLUnit XML methods
 	private static List<HtmlElement> rowsHtmlElementList;
 	private static HtmlElement rowHtmlElement;
 	private static HtmlElement priceHtmlElement;
 	private static HtmlAnchor infoHtmlAnchor;
 
-	// retrievable thru HTMLUnit CSS Selector methods
+	// collect via HTMLUnit CSS Selector methods
 	private static DomNodeList<DomNode> rowsDomNodeList;
 	private static Iterator<DomNode> rowsDomNodeIterator;
 	private static DomNode rowDomNode;
@@ -149,13 +151,11 @@ public class HtmlUnitTest extends BaseTest {
 	@BeforeClass
 	public void beforeClass() throws IOException {
 		super.setBrowser("chrome");
-		// TODO: stop the chrome browser hanging in waiting for use.typekit.net
+		// TODO: prevent Chrome browser from hanging in a wait for use.typekit.net
 		super.beforeClass();
 		assertThat(driver, notNullValue());
-
 		client.getOptions().setCssEnabled(false);
 		client.getOptions().setJavaScriptEnabled(false);
-
 	}
 
 	@BeforeMethod
@@ -164,6 +164,8 @@ public class HtmlUnitTest extends BaseTest {
 			baseUrl = "https://miami.craigslist.org/search/sss?query="
 					+ URLEncoder.encode(searchQuery, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return;
 		}
 
 		try {
@@ -173,20 +175,21 @@ public class HtmlUnitTest extends BaseTest {
 			// org.apache.http.impl.execchain.RetryExec execute
 			// INFO: Retrying request to {s}->https://miami.craigslist.org:443
 			page = client.getPage(baseUrl);
-			// NOTE: require a recent version of htmlunit
-			// with 2.25 com.gargoylesoftware.htmlunit.html.HtmlPage did not have
-			// getElementsById
+			assertThat(page, notNullValue());
+
+			// confirm the page contains the search input
+			// NOTE: requires recent version of htmlunit
+			// 2.25 and prior com.gargoylesoftware.htmlunit.html.HtmlPage did not have
+			// getElementsById method
 			inputSearch = (HtmlInput) (page.getElementsById("query").get(0));
+			assertThat(inputSearch, notNullValue());
 			pageXML = page.asXml();
+			assertThat(pageXML, notNullValue());
 		} catch (FailingHttpStatusCodeException | IOException e) {
 			e.printStackTrace();
 		}
-		assertThat(page, notNullValue());
-		// confirm the page contains the search input
-		assertThat(inputSearch, notNullValue());
-		assertThat(pageXML, notNullValue());
 		driver.navigate().to(baseUrl);
-		// HTMLUnit does not support loading page source ?
+		// TODO: doesn't HTMLUnit support loading page source ?
 		pageSource = driver.getPageSource();
 	}
 
@@ -208,7 +211,7 @@ public class HtmlUnitTest extends BaseTest {
 	@Test(enabled = true)
 	public void testSilent() {
 		rowsHtmlElementList = page.getByXPath(rowXpath);
-		// implicit cast taking place of List<Object> to List<HtmlElement>
+		// implicit cast from List<Object> to List<HtmlElement>
 		assertThat(rowsHtmlElementList, notNullValue());
 		assertThat(rowsHtmlElementList.size(), greaterThan(0));
 		rowsHtmlElementList.stream()
@@ -221,11 +224,10 @@ public class HtmlUnitTest extends BaseTest {
 		itemName = infoHtmlAnchor.asText();
 		itemUrl = infoHtmlAnchor.getHrefAttribute();
 
-		// It is possible that an item doesn't have any price
+		// item may have no price
 		itemPrice = priceHtmlElement == null ? "0.0" : priceHtmlElement.asText();
 		System.err.println(String.format("Name: %s\nPrice: %s\nUrl : %s", itemName,
 				itemPrice, itemUrl));
-
 	}
 
 	@Test(enabled = true)
@@ -251,7 +253,7 @@ public class HtmlUnitTest extends BaseTest {
 		org.w3c.dom.NamedNodeMap infoNodeAttributes = infoDomNode.getAttributes();
 		itemUrl = infoNodeAttributes.getNamedItem("href").getNodeValue();
 		priceDomNode = rowDomNode.querySelector(priceSelector);
-		// It is possible that an item doesn't have any price
+		// item may have no price
 		itemPrice = priceDomNode == null ? "0.0" : priceDomNode.asText();
 		System.err.println(String.format("Name: %s\nPrice: %s\nUrl : %s", itemName,
 				itemPrice, itemUrl));
@@ -267,57 +269,72 @@ public class HtmlUnitTest extends BaseTest {
 			// getting
 			// The method writeValueAsString(Object) from the type ObjectMapper refers
 			// to the missing type JsonProcessingException
-			jsonString = prettify
+			jsonString = prettyPrint
 					? mapper.writerWithDefaultPrettyPrinter().writeValueAsString(objItem)
 					: mapper.writeValueAsString(objItem);
 			System.err.println("Processing JSON: " + jsonString);
 			// based on https://toster.ru/q/659184
 
+			// @formatter:off
 			// Type type1 = new TypeToken<ObjectItem>().getType();
 			// The constructor TypeToken<ObjectItem>() is not visible
 			// hence the empty args
 			Type type1 = new TypeToken<ObjectItem>() { }.getType();
+			// @formatter:on
 			try {
-				// create json parser
+				// NOTE: creating fresh instance of json parser
 				Gson parser = new Gson();
 				@SuppressWarnings("unused")
 				ObjectItem data1 = parser.fromJson(jsonString, type1);
 				// parse data to
-			} catch (JsonSyntaxException e1) {
+			} catch (JsonSyntaxException e) {
 				// Expected BEGIN_OBJECT but was STRING
-				System.err.println("Exception (ignored) " + e1.toString());
+				System.err.println("Exception (ignored) " + e.toString());
 			}
 
+			// @formatter:off
 			// create custom type
-			Type type2 = new TypeToken<Map<String, ObjectItem[]>>() {
-			}.getType();
+			Type type2 = new TypeToken<Map<String, ObjectItem[]>>() {}.getType();
+			// @formatter:on
 			try {
-				// create json parser
+				// NOTE: creating fresh instance of json parser
 				Gson parser = new Gson();
 				@SuppressWarnings("unused")
-				Map<String, ObjectItem[]> data2 = parser.fromJson(jsonString, type2);
+				Map<String, ObjectItem[]> data2 = parser
+						.fromJson(String.format("{\"data\": [%s]}", jsonString), type2);
 				// parse data to
-			} catch (JsonSyntaxException e2) {
+				assertThat(data2, notNullValue());
+				assertThat(data2.keySet(), notNullValue());
+				// assert that value is an array
+				assertThat(data2.values().toArray()[0], notNullValue());
+			} catch (JsonSyntaxException e) {
 				// Expected BEGIN_ARRAY but was STRING
-				System.err.println("Exception (ignored) " + e2.toString());
+				// TODO : report line number
+				System.err.println("Exception (ignored) " + e.toString());
 			}
 			// based on https://toster.ru/q/659184
 			// create custom type
-			Type type3 = new TypeToken<Map<String, ObjectItem>>() {
-			}.getType();
+			// @formatter:off
+			Type type3 = new TypeToken<Map<String, ObjectItem>>() {}.getType();
+			// @formatter:on
+
 			try {
-				// create json parser
+				// NOTE: creating fresh instance of json parser
 				Gson parser = new Gson();
 				@SuppressWarnings("unused")
-				Map<String, ObjectItem> data3 = parser.fromJson(jsonString, type3);
+
+				Map<String, ObjectItem> data3 = parser
+						.fromJson(String.format("{\"data\": %s}", jsonString), type3);
 				// parse data to
+				assertThat(data3, notNullValue());
+				assertThat(data3.keySet(), notNullValue());
 			} catch (JsonSyntaxException e3) {
-				// Expected BEGIN_OBJECT but was STRING
+				// TODO : report line number
 				System.err.println("Exception (ignored) " + e3.toString());
+				// Expected BEGIN_OBJECT but was STRING
 			}
 		} catch (JsonProcessingException e) {
 			System.err.println("Exception (ignored) " + e.toString());
-
 		}
 		System.err.println(
 				String.format("JSON serialization with Jackson: \n%s\n", jsonString));
