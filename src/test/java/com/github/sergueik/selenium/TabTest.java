d88d9a1cf996c56945a20ac325bef0276cb24126
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-10-16 23:26:57 -0400

    intermediate revision - refactored, added failing empty new tab attempt, exercised testng annotation on expected exception class and message

--- src/test/java/com/github/sergueik/selenium/TabTest.java
+++ src/test/java/com/github/sergueik/selenium/TabTest.java
@@ -10,6 +10,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertTrue;
 
 import java.lang.reflect.Method;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -22,7 +23,7 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -38,6 +39,9 @@ public class TabTest extends BaseTest {
 	private static String baseURL = "http://stackoverflow.com/";
 	private static final StringBuffer verificationErrors = new StringBuffer();
 	private final static String cssSelector = "a.-logo";
+	private static WebElement logoElement;
+	private static String parentHandle = null;
+	private static int numberOfWindows = 0;
 
 	@BeforeMethod
 	public void BeforeMethod(Method method) {
@@ -58,34 +62,23 @@ public class TabTest extends BaseTest {
 	@Test(enabled = true)
 	public void getTabOpenTest() {
 		// Arrange
-		WebElement logoElement;
-		String parentHandle = driver.getWindowHandle(); // Save parent window
+		parentHandle = driver.getWindowHandle(); // Save parent window
+		numberOfWindows = driver.getWindowHandles().size();
 		try {
 			logoElement = wait.until(ExpectedConditions
 					.visibilityOfElementLocated(By.cssSelector(cssSelector)));
 			if (logoElement != null) {
 				actions.moveToElement(logoElement).build().perform();
-				System.err
-						.println("Ctrl-clicking on: " + logoElement.getAttribute("outerHTML"));
+				System.err.println(
+						"Ctrl-clicking on: " + logoElement.getAttribute("outerHTML"));
 				logoElement.sendKeys(Keys.chord(Keys.CONTROL, Keys.RETURN));
 			}
 			// switch to the other window now
 			boolean isChildWindowOpen = wait
 					.until(ExpectedConditions.numberOfWindowsToBe(2));
 			if (isChildWindowOpen) {
-				Set<String> handles = driver.getWindowHandles();
-				// Switch to child window
-				for (String handle : handles) {
-					driver.switchTo().window(handle);
-					if (!parentHandle.equals(handle)) {
-						driver.manage().window().maximize();
-						System.err.println("Close the extra browser tab: " + handle);
-						driver.close();
-						driver.switchTo().window(parentHandle);
-						sleep(1000);
-						break;
-					}
-				}
+				// switch to the other window now
+				walkTabs(parentHandle);
 			}
 		} catch (TimeoutException e) {
 			System.err.println("Exception (aborting) " + e.toString());
@@ -93,11 +86,39 @@ public class TabTest extends BaseTest {
 		}
 	}
 
+	@Test(enabled = true, expectedExceptions = {
+			org.openqa.selenium.TimeoutException.class }, expectedExceptionsMessageRegExp = "Expected condition failed: waiting for number of open windows to be \\d .*$")
+	public void emptyNewTabTest() {
+		// Arrange
+		parentHandle = driver.getWindowHandle(); // Save parent window
+		numberOfWindows = driver.getWindowHandles().size();
+		WebElement bodyElement = driver.findElement(By.cssSelector("body"));
+		assertThat(bodyElement, notNullValue());
+		// body Element is not null
+		if (bodyElement != null) {
+			bodyElement.sendKeys(Keys.CONTROL + "t");
+		}
+		// makes no difference
+		if (bodyElement != null) {
+			bodyElement.sendKeys(Keys.chord(Keys.CONTROL, "t"));
+		}
+
+		WebDriverWait waitShort = new WebDriverWait(driver, 3);
+		waitShort.pollingEvery(Duration.ofMillis(300));
+
+		boolean isChildWindowOpen = waitShort
+				.until(ExpectedConditions.numberOfWindowsToBe(numberOfWindows + 1));
+		if (isChildWindowOpen) {
+			// switch to the other window now
+			walkTabs(parentHandle);
+		}
+	}
+
 	@Test(enabled = true)
 	public void getLinkTargetTabOpenTest() {
 		// Arrange
-		WebElement logoElement;
-		String parentHandle = driver.getWindowHandle(); // Save parent window
+		parentHandle = driver.getWindowHandle(); // Save parent window
+		numberOfWindows = driver.getWindowHandles().size();
 		try {
 			logoElement = wait.until(ExpectedConditions
 					.visibilityOfElementLocated(By.cssSelector(cssSelector)));
@@ -114,25 +135,32 @@ public class TabTest extends BaseTest {
 				logoElement.click();
 			}
 			boolean isChildWindowOpen = wait
-					.until(ExpectedConditions.numberOfWindowsToBe(2));
+					.until(ExpectedConditions.numberOfWindowsToBe(numberOfWindows + 1));
 			if (isChildWindowOpen) {
-				Set<String> handles = driver.getWindowHandles();
-				// Switch to child window
-				for (String handle : handles) {
-					driver.switchTo().window(handle);
-					if (!parentHandle.equals(handle)) {
-						driver.manage().window().maximize();
-						System.err.println("Close the extra browser tab: " + handle);
-						driver.close();
-						driver.switchTo().window(parentHandle);
-						sleep(1000);
-						break;
-					}
-				}
+				// switch to the other window now
+				walkTabs(parentHandle);
 			}
 		} catch (TimeoutException e) {
 			System.err.println("Exception (aborting) " + e.toString());
 			return;
 		}
 	}
+
+	private void walkTabs(String parentHandle) {
+		// switch to the other window now
+		Set<String> handles = driver.getWindowHandles();
+		// Switch to child window
+		for (String handle : handles) {
+			driver.switchTo().window(handle);
+			if (!parentHandle.equals(handle)) {
+				driver.manage().window().maximize();
+				System.err.println("Close the browser tab: " + handle);
+				driver.close();
+				driver.switchTo().window(parentHandle);
+				sleep(1000);
+				break;
+			}
+		}
+	}
+
 }
