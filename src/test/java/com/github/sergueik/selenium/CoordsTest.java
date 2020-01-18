3504cbe9cf3c8b42e8d08f74f735f31f3ba8214f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-01-18 11:56:03 -0500

    intermediate revision - collapsed the redundant tests into with parameters

--- src/test/java/com/github/sergueik/selenium/CoordsTest.java
+++ src/test/java/com/github/sergueik/selenium/CoordsTest.java
@@ -1,144 +1,57 @@
 package com.github.sergueik.selenium;
 
+/**
+ * Copyright 2020 Serguei Kouzmine
+ */
+
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertTrue;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.io.File;
-import java.util.List;
-import java.util.Set;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.Key;
-import org.sikuli.script.Match;
-import org.sikuli.script.Pattern;
-import org.sikuli.script.Screen;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertTrue;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 /**
  * Selected test scenarios for Selenium WebDriver
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
-// inspred by https://qna.habr.com/q/700693
+// inspired by https://qna.habr.com/q/700693
 
 public class CoordsTest extends BaseTest {
 
-	private String baseURL;
-	private String selector;
-	private static String id;
-
-	@Test(priority = 1, enabled = true)
-	public void getCoordsTest1() {
-		baseURL = "https://www.wikipedia.org/";
-		selector = "#searchInput";
-		driver.get(baseURL);
-		WebElement element = wait.until(ExpectedConditions
-				.visibilityOf(driver.findElement(By.cssSelector(selector))));
-		assertThat(element, notNullValue());
-		String data = (String) super.executeScript(
-				super.getScriptContent("getCoords.js"), selector, "css", false);
-		System.err.println(data);
+	@DataProvider(name = "argument-provider", parallel = false)
+	public Object[][] provideArgs() throws Exception {
+		return new Object[][] {
+				{ "https://www.wikipedia.org/", "#searchInput", "css" },
+				{ "https://www.wikipedia.org/", "searchInput", "id" },
+				{ "https://ya.ru/", "button.button", "css" },
+				{ "https://ya.ru/", "#text", "css" },
+				{ "https://ya.ru/", "text", "id" } };
 	}
 
-	@Test(priority = 1, enabled = true)
-	public void getCoordsTest2() {
-		baseURL = "https://www.wikipedia.org/";
-		id = "searchInput";
+	private static WebElement element;
+
+	@Test(enabled = true, dataProvider = "argument-provider", threadPoolSize = 2)
+	public void getCoordsTest(String baseURL, String selector, String kind) {
 		driver.get(baseURL);
-		WebElement element = wait
-				.until(ExpectedConditions.visibilityOf(driver.findElement(By.id(id))));
+		if (kind.matches("id")) {
+			element = wait.until(
+					ExpectedConditions.visibilityOf(driver.findElement(By.id(selector))));
+		} else {
+			element = wait.until(ExpectedConditions
+					.visibilityOf(driver.findElement(By.cssSelector(selector))));
+		}
 		assertThat(element, notNullValue());
 		String data = (String) super.executeScript(
-				super.getScriptContent("getCoords.js"), id, "id", false);
+				super.getScriptContent("getCoords.js"), selector, kind, false);
 		System.err.println(data);
 		sleep(100);
-	}
-
-	@Test(priority = 3, enabled = true)
-	public void getCoordsTest3() {
-		baseURL = "https://ya.ru/";
-		selector = "button.button";
-		driver.get(baseURL);
-		WebElement element = wait.until(ExpectedConditions
-				.visibilityOf(driver.findElement(By.cssSelector(selector))));
-		assertThat(element, notNullValue());
-		String data = (String) super.executeScript(
-				super.getScriptContent("getCoords.js"), selector, "css", false);
+		data = (String) super.executeScript(super.getScriptContent("getCoords.js"),
+				element, "", false);
 		System.err.println(data);
 	}
 
-	@Test(priority = 4, enabled = true)
-	public void getCoordsTest4() {
-		baseURL = "https://ya.ru/";
-		selector = "#text";
-		driver.get(baseURL);
-		WebElement element = wait.until(ExpectedConditions
-				.visibilityOf(driver.findElement(By.cssSelector(selector))));
-		assertThat(element, notNullValue());
-		String data = (String) super.executeScript(
-				super.getScriptContent("getCoords.js"), selector, "css", false);
-		System.err.println(data);
-	}
-
-	@Test(priority = 5, enabled = true)
-	public void getCoordsTest5() {
-		baseURL = "https://ya.ru/";
-		id = "text";
-		driver.get(baseURL);
-		WebElement element = wait
-				.until(ExpectedConditions.visibilityOf(driver.findElement(By.id(id))));
-		assertThat(element, notNullValue());
-		String data = (String) super.executeScript(
-				super.getScriptContent("getCoords.js"), id, "id", false);
-		System.err.println(data);
-	}
-
-	@Test(priority = 6, enabled = true)
-	public void getCoordsTest6() {
-		baseURL = "https://ya.ru/";
-		id = "text";
-		driver.get(baseURL);
-		WebElement element = wait
-				.until(ExpectedConditions.visibilityOf(driver.findElement(By.id(id))));
-		assertThat(element, notNullValue());
-		String data = (String) super.executeScript(
-				super.getScriptContent("getCoords.js"), element, "", false);
-		System.err.println(data);
-	}
 }
