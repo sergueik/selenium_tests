75b429829c5576ce2303c61747264e3274d326ee
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-11 16:54:29 -0400

    intermediate revision - cleanup of the code, best practices

--- src/test/java/com/github/sergueik/selenium/CloudCalculatorTest.java
+++ src/test/java/com/github/sergueik/selenium/CloudCalculatorTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,13 +25,37 @@ import org.testng.annotations.Test;
 
 /**
  * Selected test scenarios for Selenium WebDriver
+ * Find Angular-generated input and  select elements by their id using the HTML5 element-to-element
+ * relationship attributes, and exercising the ng-click workaround - only one works
  * 
  * @author: Serguei Kouzmine (kouzmine_serguei@yahoo.com)
  */
 
 public class CloudCalculatorTest extends BaseTest {
 
+	private static boolean debug = true;
 	private static String baseURL = "https://cloud.google.com/products/calculator";
+
+	private static WebDriver iframe = null;
+	private static WebDriver nestedIframe = null;
+	private static final String clusterPurpose = "testing";
+	private static List<WebElement> iframes = new ArrayList<>();
+	private static Map<String, Object> iframesMap = new HashMap<>();
+	private static List<WebElement> inputLabels = new ArrayList<>();
+	private static List<WebElement> selectLabels = new ArrayList<>();
+	private static WebElement inputLabel = null;
+	private static WebElement inputElement = null;
+	private static String ownedAreaId = null;
+	private static String optionId = null;
+	private static String containerId = null;
+	private static WebElement selectLabel = null;
+	private static WebElement selectElement = null;
+	private static WebElement selectValue = null;
+	private static String optionText = null;
+	private static WebElement optionElement = null;
+
+	private static WebElement frameElement = null;
+	private static WebElement element = null;
 
 	@BeforeClass
 	public void beforeClass() throws IOException {
@@ -56,36 +81,44 @@ public class CloudCalculatorTest extends BaseTest {
 		driver.get(baseURL);
 	}
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void testFramePresent() {
-		List<WebElement> iframes = driver
-				.findElements(By.cssSelector("devsite-iframe iframe"));
-		Map<String, Object> iframesMap = new HashMap<>();
-		for (WebElement iframe : iframes) {
-			String key = String.format("id: \'%s\", name: \"%s\"",
-					iframe.getAttribute("id"), iframe.getAttribute("name"));
-			System.err.println(String.format("Found iframe %s", key));
-			iframesMap.put(key, iframe);
+		iframes.clear();
+		iframes = driver.findElements(By.cssSelector("devsite-iframe iframe"));
+		iframesMap = new HashMap<>();
+		for (WebElement element : iframes) {
+			String key = String.format("{\"key\":\"name\",\"value\":\"%s\"}",
+					element.getAttribute("name"));
+			if (debug) {
+				System.err.println(String.format("Found iframe %s:\n%s", key,
+						element.getAttribute("outerHTML")));
+			}
+			iframesMap.put(key, element);
 		}
 	}
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void testNestedFramePresent() {
 
-		WebElement element = wait
-				.until(ExpectedConditions.visibilityOfElementLocated(
-						By.cssSelector("devsite-iframe iframe[name^='goog_']")));
-		assertThat(element, notNullValue());
+		frameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.cssSelector("devsite-iframe iframe[name^='goog_']")));
+		assertThat(frameElement, notNullValue());
 
-		WebDriver iframe = driver.switchTo().frame(element);
-		// System.err.println("Frame source:" + iframe.getPageSource());
-		List<WebElement> iframes = iframe.findElements(By.cssSelector("iframe"));
-		Map<String, Object> iframesMap = new HashMap<>();
-		for (WebElement nestedIframe : iframes) {
-			String key = String.format("id: \'%s\", src: \"%s\"",
-					nestedIframe.getAttribute("id"), nestedIframe.getAttribute("src"));
-			System.err.println(String.format("Found iframe (depth 2) %s", key));
-			iframesMap.put(key, nestedIframe);
+		iframe = driver.switchTo().frame(frameElement);
+		if (debug) {
+			// System.err.println("Frame source:" + iframe.getPageSource());
+		}
+		iframes.clear();
+		iframes = iframe.findElements(By.cssSelector("iframe"));
+		iframesMap = new HashMap<>();
+		for (WebElement element : iframes) {
+			String key = String.format("{\"key\":\"id\",\"value\":\"%s\"}",
+					element.getAttribute("id"));
+			if (debug) {
+				System.err.println(String.format("Found iframe %s:\n%s", key,
+						element.getAttribute("outerHTML")));
+			}
+			iframesMap.put(key, element);
 		}
 		driver.switchTo().defaultContent();
 
@@ -94,130 +127,112 @@ public class CloudCalculatorTest extends BaseTest {
 	@Test(enabled = true)
 	public void testSwitchIntoNestedPage() {
 
-		WebElement element = wait
-				.until(ExpectedConditions.visibilityOfElementLocated(
-						By.cssSelector("devsite-iframe iframe[name^='goog_']")));
-		assertThat(element, notNullValue());
+		frameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.cssSelector("devsite-iframe iframe[name^='goog_']")));
+		assertThat(frameElement, notNullValue());
 
-		WebDriver iframe = driver.switchTo().frame(element);
-		element = iframe.findElement(By.cssSelector("iframe:nth-of-type(1)"));
-		WebDriver nestedIframe = iframe.switchTo().frame(element);
-		// System.err.println("Frame source:" + nestedIframe.getPageSource());
-
-		List<WebElement> inputLabels = nestedIframe
-				.findElements(By.cssSelector("label[for^=\"input_\"]"));
-		WebElement input1Label = inputLabels.stream()
+		iframe = driver.switchTo().frame(frameElement);
+		// get first iframe element - no good attribute to use
+		frameElement = iframe.findElement(By.cssSelector("iframe:nth-of-type(1)"));
+		assertThat(frameElement, notNullValue());
+		nestedIframe = iframe.switchTo().frame(frameElement);
+		if (debug) {
+			// System.err.println("Frame source:" + nestedIframe.getPageSource());
+		}
+		inputLabels = nestedIframe
+				.findElements(By.cssSelector("label[for^='input_']"));
+		inputLabel = inputLabels.stream()
 				.filter(o -> o.getText().contains((CharSequence) "Number of instances"))
 				.collect(Collectors.toList()).get(0);
-		// System.err
-		// .println("Input label source: " + input1Label.getAttribute("outerHTML"));
-		WebElement input1 = nestedIframe.findElement(By.cssSelector(
-				String.format("input[id=\"%s\"]", input1Label.getAttribute("for"))));
-		input1.sendKeys("1");
+		if (debug) {
+			System.err.println(
+					"Input label source: " + inputLabel.getAttribute("outerHTML"));
+		}
+		inputElement = nestedIframe.findElement(By.cssSelector(
+				String.format("input[id='%s']", inputLabel.getAttribute("for"))));
+		inputElement.sendKeys("1");
 		sleep(100);
 
 		inputLabels = nestedIframe
-				.findElements(By.cssSelector("label[for^=\"input_\"]"));
-		WebElement input2Label = inputLabels.stream()
+				.findElements(By.cssSelector("label[for^='input_']"));
+		inputLabel = inputLabels.stream()
 				.filter(o -> o.getText()
 						.contains((CharSequence) "What are these instances for"))
 				.collect(Collectors.toList()).get(0);
-		// System.err
-		// .println("Input label source: " +
-		// input2Label.getAttribute("outerHTML"));
-		WebElement input2 = nestedIframe.findElement(By.cssSelector(
-				String.format("input[id=\"%s\"]", input2Label.getAttribute("for"))));
-		input2.sendKeys("testing");
+		if (debug) {
+			System.err.println(
+					"Input label source: " + inputLabel.getAttribute("outerHTML"));
+		}
+		inputElement = nestedIframe.findElement(By.cssSelector(
+				String.format("input[id='%s']", inputLabel.getAttribute("for"))));
+		inputElement.sendKeys(clusterPurpose);
 		sleep(100);
 
-		List<WebElement> selectLabels = nestedIframe
-				.findElements(By.cssSelector("label[for^=\"select_\"]"));
-		WebElement select1Label = selectLabels.stream()
+		selectLabels = nestedIframe
+				.findElements(By.cssSelector("label[for^='select_']"));
+		selectLabel = selectLabels.stream()
 				.filter(o -> o.getText().contains((CharSequence) "Operating System"))
 				.collect(Collectors.toList()).get(0);
-		String containerId = select1Label.getAttribute("for");
-		System.err.println(
-				"Select label source: " + select1Label.getAttribute("outerHTML"));
-		WebElement select1 = nestedIframe.findElement(
-				By.cssSelector(String.format("md-select[id=\"%s\"]", containerId)));
+		containerId = selectLabel.getAttribute("for");
+		if (debug) {
+			System.err.println(
+					"Select label source: " + selectLabel.getAttribute("outerHTML"));
+		}
+		selectElement = nestedIframe.findElement(
+				By.cssSelector(String.format("md-select[id='%s']", containerId)));
 
-		System.err.println("Select source: " + select1.getAttribute("outerHTML"));
-
-		String ownedAreaId = select1.getAttribute("aria-owns");
-		WebElement selectValue = select1
+		if (debug) {
+			System.err
+					.println("Select source: " + selectElement.getAttribute("outerHTML"));
+		}
+		ownedAreaId = selectElement.getAttribute("aria-owns");
+		selectValue = selectElement
 				.findElement(By.cssSelector("md-select-value > span > div"));
 		assertThat(selectValue, notNullValue());
+		if (debug) {
+			System.err.println(String.format("Simulate mouse click on element \"%s\"",
+					selectElement.getText()));
+		}
 		// https://stackoverflow.com/questions/32499174/selenium-click-event-does-not-trigger-angularjs-ng-click
-		System.err.println("Simulate mouse click on element");
+		// NOTE: the basic click() or Actions do not work
+		// (new Actions(nestedIframe)).moveToElement(selectValue).click().perform();
 		executeScript("arguments[0].click();", selectValue);
 		highlight(selectValue, 1000, "solid red");
-		sleep(1000);
-		// NOTE: even after fixing the driver argument with embedded iframe,
-		// the Actions does not work
-		/*
-		Actions action = new Actions(nestedIframe);
-		action.moveToElement(selectValue).perform();
-		action.moveToElement(selectValue).click().perform();
-		
-		highlight(selectValue, 1000, "solid black");
-		*/
-		select1 = nestedIframe.findElement(By.cssSelector(String
-				.format("md-select[id=\"%s\"]", select1Label.getAttribute("for"))));
-		System.err.println("Select source (options visible): "
-				+ select1.findElement(By.xpath("..")).getAttribute("outerHTML"));
 
-		String findText = "Windows Server 2008r2";
-		String targetId = "select_option_58";
-		WebElement option2 = null;
-		try {
-			option2 = nestedIframe.findElement(By.xpath(String.format(
-					"//*[@id=\"%s\"]//md-select-menu//md-select//md_option[contains(text(), \"%s\")]",
-					ownedAreaId, findText)));
-			// System.err.println(
-			// "Selecting different option: " + option2.getAttribute("outerHTML"));
-		} catch (org.openqa.selenium.NoSuchElementException e) {
-			// ignore
-			System.err.println("Exception (ignord) : " + e.toString());
+		// Find Angular-generated select element by id using HTML5 element
+		// relationship attributes
+		selectElement = nestedIframe.findElement(By.cssSelector(
+				String.format("md-select[id='%s']", selectLabel.getAttribute("for"))));
+		if (debug) {
+			System.err.println("Select source (options visible): " + selectElement
+					.findElement(By.xpath("..")).getAttribute("outerHTML"));
 		}
-		try {
-			assertThat(option2, notNullValue());
-			targetId = option2.getAttribute("id");
-		} catch (AssertionError e) {
-			// ignore
+
+		optionText = "Windows Server 2008r2";
+		String xpath2 = String.format(
+				"//*[@id='%s']//md-option[@id][div[contains(text(), '%s')]]",
+				ownedAreaId, optionText);
+		if (debug) {
+			System.err.println("Locating DOM using xpath: " + xpath2);
 		}
+		WebElement option2 = nestedIframe.findElement(By.xpath(xpath2));
+		if (debug) {
+			System.err.println(
+					"Selecting different option: " + option2.getAttribute("outerHTML"));
+		}
+		assertThat(option2, notNullValue());
+		optionId = option2.getAttribute("id");
 		WebElement option3 = nestedIframe
-				.findElement(By.cssSelector(String.format("#%s", targetId)));
+				.findElement(By.cssSelector(String.format("#%s", optionId)));
 		assertThat(option3, notNullValue());
 		// https://stackoverflow.com/questions/32499174/selenium-click-event-does-not-trigger-angularjs-ng-click
-		System.err.println("Simulate mouse click on element " + option3.getText()
-				+ " " + option3.getAttribute("id") + " "
-				+ option3.getAttribute("outerHTML"));
-
+		if (debug) {
+			System.err.println(String.format(
+					"Simulate mouse click on chosen option \"%s\"", option3.getText()));
+		}
 		executeScript("arguments[0].click();", option3);
 		sleep(1000);
 
-		// highlight(selectValue, 1000, "solid red");
-
-		/*
-				WebElement menu1 = select1
-						.findElement(By.xpath("div[@class=\"md-select-menu-container\"]"));
-				assertThat(menu1, notNullValue());
-				System.err.println(
-						"Select menu container source: " + menu1.getAttribute("outerHTML"));
-		
-				WebElement value1 = menu1.findElement(By.xpath(
-						"md-select-menu/md-content/md-option[@selected=\"selected\"]/div"));
-				assertThat(value1, notNullValue());
-				System.err
-						.println("Selected option source: " + value1.getAttribute("outerHTML"));
-		
-				WebElement value2 = menu1.findElement(By.xpath(
-						"md-select-menu/md-content/md-option[@aria-selected=\"false\"][1]"));
-				System.err.println(
-						"Not selected option source: " + value2.getAttribute("outerHTML"));
-				value2.click();
-				sleep(10000);
-				*/
 		iframe.switchTo().defaultContent();
 		driver.switchTo().defaultContent();
 
