55252bbce307de9fc786088ae52576f52935f508
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-12 02:04:03 -0400

    itermediate revision - added refactoring, utility to pass code around via json

--- src/test/java/com/github/sergueik/selenium/CloudCalculatorTest.java
+++ src/test/java/com/github/sergueik/selenium/CloudCalculatorTest.java
@@ -1,15 +1,16 @@
 package com.github.sergueik.selenium;
 
-// The import org.hamcrest.Matchers.matchesRegex cannot be resolved
-// import static org.hamcrest.Matchers.matchesRegex;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -23,6 +24,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Selected test scenarios for Selenium WebDriver
@@ -42,6 +46,7 @@ public class CloudCalculatorTest extends BaseTest {
 
 	private final static String xpathTemplate = "//*[@id='%s']//md-option[@id][div[contains(text(), '%s')]]";
 	private static String xpath;
+	private static String frameName = null;
 	private static WebDriver iframe = null;
 	private static WebDriver nestedIframe = null;
 	private static final String clusterPurpose = "testing";
@@ -88,7 +93,7 @@ public class CloudCalculatorTest extends BaseTest {
 		driver.get(baseURL);
 	}
 
-	@Test(enabled = true)
+	@Test(enabled = false)
 	public void testFramePresent() {
 		iframes.clear();
 		iframes = driver.findElements(By.cssSelector("devsite-iframe iframe"));
@@ -131,18 +136,87 @@ public class CloudCalculatorTest extends BaseTest {
 
 	}
 
+	@Test(enabled = false)
+	public void testSwitchIntoNestedFrame() {
+
+		iframesMap = new HashMap<>();
+		for (WebElement element : driver
+				.findElements(By.cssSelector("devsite-iframe iframe"))) {
+			iframesMap.put(String.format("{\"key\":\"name\",\"value\":\"%s\"}",
+					element.getAttribute("name")), element);
+		}
+		assertThat(iframesMap.keySet().isEmpty(), is(false));
+		for (String key : iframesMap.keySet()) {
+			if (debug) {
+				System.err.println(String.format("Found iframe %s:\n%s", key,
+						((WebElement) iframesMap.get(key)).getAttribute("outerHTML")));
+			}
+			if (frameName == null) {
+				frameName = key;
+			}
+		}
+		Map<String, String> collector = new HashMap<>();
+		String result = readData(frameName, Optional.of(collector));
+		if (debug) {
+			System.err.println(
+					"key: " + collector.get("key") + " value: " + collector.get("value"));
+		}
+		frameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
+				By.xpath(String.format("//devsite-iframe//iframe[@%s='%s']",
+						collector.get("key"), collector.get("value")))));
+		assertThat(frameElement, notNullValue());
+
+		// get nested iframe elements by id
+		iframesMap = new HashMap<>();
+		for (WebElement element : (driver.switchTo().frame(frameElement))
+				.findElements(By.cssSelector("iframe"))) {
+			String key = String.format("{\"key\":\"id\",\"value\":\"%s\"}",
+					element.getAttribute("id"));
+			if (debug) {
+				System.err.println(String.format("Found iframe %s:\n%s", key,
+						element.getAttribute("outerHTML")));
+			}
+			iframesMap.put(key, element);
+		}
+		assertThat(iframesMap.keySet().isEmpty(), is(false));
+		for (String key : iframesMap.keySet()) {
+			if (debug) {
+				System.err.println(String.format("Found iframe %s:\n%s", key,
+						((WebElement) iframesMap.get(key)).getAttribute("outerHTML")));
+			}
+			if (frameName == null) {
+				frameName = key;
+			}
+		}
+		collector = new HashMap<>();
+		result = readData(frameName, Optional.of(collector));
+		if (debug) {
+			System.err.println(
+					"key: " + collector.get("key") + " value: " + collector.get("value"));
+		}
+		/*
+		element = (driver.switchTo().frame(frameElement)).findElement(
+				By.xpath(String.format("//devsite-iframe//iframe[@%s='%s']",
+						collector.get("key"), collector.get("value"))));
+		assertThat(element, notNullValue());
+		*/
+	}
+
 	@Test(enabled = true)
-	public void testSwitchIntoNestedPage() {
+	public void testElements() {
 
 		frameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(
 				By.cssSelector("devsite-iframe iframe[name^='goog_']")));
 		assertThat(frameElement, notNullValue());
 
+		// get nested iframe element by id? NPE
+		// keep finding first of
+		// element = (driver.switchTo().frame(frameElement))
+		// .findElement(By.cssSelector("iframe#myFrame"));
 		iframe = driver.switchTo().frame(frameElement);
-		// get first iframe element - no good attribute to use
-		frameElement = iframe.findElement(By.cssSelector("iframe:nth-of-type(1)"));
-		assertThat(frameElement, notNullValue());
-		nestedIframe = iframe.switchTo().frame(frameElement);
+		element = iframe.findElement(By.cssSelector("iframe:nth-of-type(1)"));
+		assertThat(element, notNullValue());
+		nestedIframe = iframe.switchTo().frame(element);
 		if (debug) {
 			// System.err.println("Frame source:" + nestedIframe.getPageSource());
 		}
@@ -304,6 +378,57 @@ public class CloudCalculatorTest extends BaseTest {
 		iframe.switchTo().defaultContent();
 		driver.switchTo().defaultContent();
 
+	}
+
+	public String readData(Optional<Map<String, String>> parameters) {
+		return readData(null, parameters);
+	}
+
+	public String readData(String payload,
+			Optional<Map<String, String>> parameters) {
+		return readData(payload, parameters,
+				"(?:key|value)" /* "(?:id|name|src)" */);
+	}
+
+	// Deserialize the tiny hashmap from the JSON
+	public String readData(String payload,
+			Optional<Map<String, String>> parameters, String acceptedKeys) {
+		if (debug) {
+			System.err.println("Accepted keys: " + acceptedKeys);
+		}
+
+		Map<String, String> collector = (parameters.isPresent()) ? parameters.get()
+				: new HashMap<>();
+
+		String data = (payload == null) ? "{\"id\":\"42\"]}" : payload;
+		if (debug) {
+			System.err.println("Processing payload: " + data.replaceAll(",", ",\n"));
+		}
+		try {
+			JSONObject elementObj = new JSONObject(data);
+			@SuppressWarnings("unchecked")
+			Iterator<String> propIterator = elementObj.keys();
+			while (propIterator.hasNext()) {
+
+				String propertyKey = propIterator.next();
+				System.err.println(
+						((propertyKey.matches(acceptedKeys)) ? "Processing" : "Ignoring")
+								+ " key: " + propertyKey);
+				if (!propertyKey.matches(acceptedKeys)) {
+					continue;
+				}
+				String propertyVal = (String) elementObj.getString(propertyKey);
+				if (debug) {
+					System.err
+							.println("Loaded string: " + propertyKey + ": " + propertyVal);
+				}
+				collector.put(propertyKey, propertyVal);
+			}
+		} catch (JSONException e) {
+			System.err.println("Exception (ignored, aborting): " + e.toString());
+			return null;
+		}
+		return (String) collector.get("id");
 	}
 
 }
