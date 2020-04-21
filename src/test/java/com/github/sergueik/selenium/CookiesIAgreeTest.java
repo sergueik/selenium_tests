b8ff8d62bf801be21d3da2e5634110d51571a5c2
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-04-21 02:47:42 +0200

    intermediate revision -  added Cookie Agree button and partial tab opener test

---
+++ src/test/java/com/github/sergueik/selenium/CookiesIAgreeTest.java
@@ -0,0 +1,116 @@
+package com.github.sergueik.selenium;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+// 
+public class CookiesIAgreeTest extends BaseTest {
+
+	private static String baseURL = "https://link.testproject.io/vr9";
+	private static WebElement element = null;
+	private static List<WebElement> elements = new ArrayList<>();
+	private final static boolean debug = false;
+
+	@BeforeMethod
+	public void BeforeMethod() {
+		driver.get(baseURL);
+	}
+
+	@Test(enabled = true)
+	public void selectCookieMessageXPathTest() {
+		element = driver.findElement(By.id("cookie-law-info-bar"));
+		assertThat(element, notNullValue());
+		highlight(element, 100);
+		if (debug) {
+			System.err.println(element.getAttribute("innerHTML"));
+		}
+
+		// "I AGREE" button
+		elements = driver.findElements(By.xpath("//a[contains(@class, 'button')]"));
+		assertThat(elements.size(), greaterThan(0));
+		// show every button. NOTE: time-consuming
+		elements.stream().forEach(o -> {
+			// NOTE: ignoring TimeoutException from highlight(o) and continue is quite
+			// time consuming
+			/*  often text element text appears empty */
+			System.err.println(
+					"text: " + o.getText() + "html: " + o.getAttribute("outerHTML"));
+			try {
+				o.sendKeys(Keys.CONTROL + "t"); // no effect ?
+			} catch (ElementNotInteractableException e) {
+				System.err.println("Exception (ignored): " + e.toString());
+
+			}
+		});
+		element = driver.findElement(By.id("cc-button"));
+		assertThat(element, notNullValue());
+		highlight(element, 1000);// no exception but no visual effect
+		if (debug) {
+			System.err.println("text: " + element.getText() + "html: "
+					+ element.getAttribute("outerHTML"));
+		}
+		element = driver.findElements(By.xpath("//a[contains(@class, 'button')]"))
+				.stream()
+				.filter(o -> o.getText().trim().compareToIgnoreCase("I AGREE") > -1)
+				.collect(Collectors.toList()).get(0);
+		assertThat(element, notNullValue());
+		element.sendKeys(Keys.CONTROL + "t"); // no effect
+		// NOTE: the following xpath locator failing to find anything despite labor
+		// intensive tweaks
+		/*
+		 "//a[contains(@class, 'button')][contains(translate(normalize-space(text()), ' ', ''),'IAGREE')]"
+		 "//a[contains(@class, 'button')][contains(normalize-space(text()),'IAGREE')]"
+		 "//a[contains(@class, 'button')][contains(text(),'IAGREE')]"
+		 */
+		try {
+			element = driver.findElement(By.xpath(
+					"//a[contains(@class, 'button')][contains(translate(normalize-space(text()), ' ', ''),'IAGREE')]"));
+			assertThat(element, notNullValue());
+			//
+			element.sendKeys(Keys.CONTROL + "t"); // no effect
+			highlight(element, 1000);
+			if (debug) {
+				System.err.println("text: " + element.getText() + "html: "
+						+ element.getAttribute("outerHTML"));
+			}
+		} catch (NoSuchElementException e) {
+			System.err.println("Exception (ignored): " + e.toString());
+		}
+		// "Learn more" link
+		element = driver
+				.findElement(By.xpath("//a[@href][contains(text(),\"Learn more\")]"));
+		assertThat(element, notNullValue());
+		// org.openqa.selenium.ElementNotInteractableException:
+		// NOTE; TimeoutException when highlight is tried
+		// highlight(agreeButtonElement, 1000);
+		if (debug) {
+			System.err.println(element.getAttribute("outerHTML"));
+		}
+		element = driver.findElement(By.tagName("body"));
+		if (debug) {
+			System.err.println(element.getAttribute("tagName"));
+		}
+		// based on:
+		// https://stackoverflow.com/questions/17547473/how-to-open-a-new-tab-using-selenium-webdriver
+
+		element.sendKeys(Keys.CONTROL + "t"); // no effect
+		// actions.keyDown(Keys.CONTROL).build().perform();
+		// actions.keyUp(Keys.CONTROL).build().perform();
+		// actions.keyDown().build().perform();
+		sleep(10000);
+
+	}
+}
