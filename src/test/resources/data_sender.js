23cecbf3e4982946ed0090a8f71197d7ff5bd765
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-02-04 02:47:37 +0100

    intermediate revision - added ignorin gnegative values in calculated page timings and updated consumer  to take long anyway

--- src/test/resources/data_sender.js
+++ src/test/resources/data_sender.js
@@ -1,6 +1,6 @@
 add_element = function() {
   const id = 'data';
-  if (document.getElementById(id) == null) { 
+  if (document.getElementById(id) == null) {
     element = document.createElement('span');
     element.id = id;
     element.style.display = 'none';
@@ -14,82 +14,98 @@ add_element();
 
 // based on: https://github.com/sunnylost/navigation-timing
 get_load = function(options = {}) {
-    var phases = [{
-        name: 'Redirect',
-        start: 'redirectStart',
-        end: 'redirectEnd',
-        index: 0
-    }, {
-        name: 'App cache',
-        start: 'fetchStart',
-        end: 'domainLookupStart',
-        index: 1
-    }, {
-        name: 'DNS',
-        start: 'domainLookupStart',
-        end: 'domainLookupEnd',
-        index: 2
-    }, {
-        name: 'TCP',
-        start: 'connectStart',
-        end: 'connectEnd',
-        index: 3
-    }, {
-        name: 'Request',
-        start: 'requestStart',
-        end: 'responseStart',
-        index: 4
-    }, {
-        name: 'Response',
-        start: 'responseStart',
-        end: 'responseEnd',
-        index: 5
-    }, {
-        name: 'Processing',
-        start: 'domLoading',
-        end: 'domComplete',
-        index: 6
-    }, {
-        name: 'onLoad',
-        start: 'loadEventStart',
-        end: 'loadEventEnd',
-        index: 7
-    }];
+  var phases = [{
+    name: 'Redirect',
+    start: 'redirectStart',
+    end: 'redirectEnd',
+    index: 0
+  }, {
+    name: 'App cache',
+    start: 'fetchStart',
+    end: 'domainLookupStart',
+    index: 1
+  }, {
+    name: 'DNS',
+    start: 'domainLookupStart',
+    end: 'domainLookupEnd',
+    index: 2
+  }, {
+    name: 'TCP',
+    start: 'connectStart',
+    end: 'connectEnd',
+    index: 3
+  }, {
+    name: 'Request',
+    start: 'requestStart',
+    end: 'responseStart',
+    index: 4
+  }, {
+    name: 'Response',
+    start: 'responseStart',
+    end: 'responseEnd',
+    index: 5
+  }, {
+    name: 'Processing',
+    start: 'domLoading',
+    end: 'domComplete',
+    index: 6
+  }, {
+    name: 'onLoad',
+    start: 'loadEventStart',
+    end: 'loadEventEnd',
+    index: 7
+  }];
 
-    var totalCost = 0;
-    var t = performance.timing;
+  var totalCost = 0;
+  var t = performance.timing;
+  var content = [];
+  // visualization code removed 
+
+  phases.forEach(function(v) {
+    var start = t[v.start],
+      end = t[v.end];
+    totalCost += (v.value = (start == 0 ? 0 : (end - start)));
+  });
+  if (options['ladder']) {
+    phases.sort(function(a, b) {
+      return b.value - a.value;
+    })
+
+    phases.forEach(function(v, i) {
+      v.width = (100 * v.value / totalCost).toFixed(3);
+    })
+
+    phases.sort(function(a, b) {
+      return a.index - b.index;
+    })
+
     var content = [];
-    // visualization code removed 
-
+    var left = 0;
     phases.forEach(function(v) {
-        var start = t[v.start],
-            end = t[v.end];
-        totalCost += (v.value = (start == 0 ? 0 : (end - start)));
-    });
-    if (options['ladder']) {
-        phases.sort(function(a, b) {
-            return b.value - a.value;
-        })
+      v.left = left;
+      left += +v.width;
+    })
+  }
+  phases.forEach(function(v) {
 
-        phases.forEach(function(v, i) {
-            v.width = (100 * v.value / totalCost).toFixed(3);
-        })
+    const parsed = parseInt(v.value, 10);
+    if (isNaN(parsed) || parsed < 0) {
+      console.log('fixing ' + v.name + ' = ' + v.value);
 
-        phases.sort(function(a, b) {
-            return a.index - b.index;
-        })
-
-        var content = [];
-        var left = 0;
-        phases.forEach(function(v) {
-            v.left = left;
-            left += +v.width;
-        })
+      v.value = 0;
     }
-    data = JSON.stringify(phases);
-    document.getElementById('data').innerHTML = data;
-    if (options['debug'] === true) {
-        confirm('Child sends:' + data);
-    }
+    // discard negative values
+    // observed NumberFormatException on Linux
+    // Expected an int but was -1612400986046 at line 1 column 604 path $[6].value
+    // Java only supports signed longs.
+
+  });
+  data = JSON.stringify(phases);
+  document.getElementById('data').innerHTML = data;
+  if (options['debug'] === true) {
+    confirm('Child sends:' + data);
+  }
 };
-get_load({'debug':true});
+get_load({
+  'debug': true
+});
